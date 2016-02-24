c5e665fc2a56c54d2c6656f7460e0b77dabbda4e
mtbrown <mark@mtbrown.me>
2016-02-23 20:49:43 -0800

    Initial project structure and system information page

---
+++ panel/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask.ext.socketio import SocketIO
+import eventlet
+
+
+# monkey patching is required because background threads are used
+eventlet.monkey_patch()
+
+socketio = SocketIO()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.debug = True
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+socketio.init_app(app)
+
+# Import views and socketio listeners, keep at bottom to avoid circular imports
+import panel.systeminfo
---
+++ panel/systeminfo.py
@@ -0,0 +1,72 @@
+import psutil
+import threading
+
+from panel import socketio, app
+from flask import render_template
+
+
+def humanize(num, suffix='B'):
+    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+        if abs(num) < 1024.0:
+            return "%3.2f %s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.2f%s%s" % (num, 'Y', suffix)
+
+
+# Socket IO thread and listeners
+@app.route('/system')
+def system():
+    return render_template('system.html',
+                           num_cpus=psutil.cpu_count(),
+                           mem_total=humanize(psutil.virtual_memory().total))
+
+
+class SystemInfoThread(threading.Thread):
+    def __init__(self):
+        super(SystemInfoThread, self).__init__()
+        self._stop = threading.Event()
+
+    @staticmethod
+    def cpu_usage():
+        return psutil.cpu_percent(interval=1, percpu=True)
+
+    @staticmethod
+    def mem_usage():
+        mem_info = psutil.virtual_memory()
+        return humanize(mem_info.used), mem_info.percent
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.is_set()
+
+    def run(self):
+        while not self._stop.is_set():
+            info = {
+                'cpu': self.cpu_usage(),
+                'mem': self.mem_usage()
+            }
+
+            socketio.emit('info', info, namespace="/sysinfo")
+        print("Stopping")
+
+
+thread = None
+
+
+@socketio.on('connect', namespace="/sysinfo")
+def connect():
+    global thread
+    print("Client connected")
+
+    print("Starting thread")
+    thread = SystemInfoThread()
+    thread.start()
+
+
+@socketio.on('disconnect', namespace="/sysinfo")
+def disconnect():
+    global thread
+    print("Client disconnected")
+    thread.stop()
