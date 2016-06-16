18c554a5a1df081be0806f6d80a66791cd1fafe8
Mark Brown <mark@mtbrown.me>
2016-06-16 13:41:40 -0700

    Added plugin loading

--- dash.py
+++ dash.py
@@ -1,4 +1,5 @@
-from dash import app, socketio
+from dash import app, socketio, plugins
 
 if __name__ == "__main__":
+    plugins.load_plugins()
     socketio.run(app)
--- dash/plugins.py
+++ dash/plugins.py
@@ -1,2 +1,15 @@
-import enum
+import sys
+import os
 
+plugins_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "plugins"))  # ../plugins
+_plugins = {}
+
+
+def load_plugins():
+    sys.path.insert(0, plugins_path)
+    for f in os.listdir(plugins_path):
+        fname, ext = os.path.splitext(f)
+        if ext == '.py':
+            mod = __import__(fname)
+            _plugins[fname] = mod.main
+    sys.path.pop(0)
