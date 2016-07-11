8e7515b1ac647bf82cd649ac9632e8d852002282
Mark Brown <mark@mtbrown.me>
2016-07-10 18:48:33 -0700

    Refactored shared chart logic to base Chart class

--- dash/components/charts.py
+++ dash/components/charts.py
@@ -1,29 +1,19 @@
+import abc
 from .panel import Panel
 from flask import render_template
 
 
-class BarChart(Panel):
-    chart_type = 'bar'
+class Chart(Panel):
+    __metaclass__ = abc.ABCMeta
 
-    def __init__(self, title=None, labels=None):
-        super(BarChart, self).__init__()
-        self.title = title
-        self.labels = labels
-        self.max_points = 0
-
-    def render_html(self):
-        return render_template('chart.html', id=self.id)
-
-    def render_js(self):
-        return render_template('chart.js', id=self.id, chart_type=self.chart_type, labels=self.labels,
-                               max_points=self.max_points)
-
-
-class LineChart(Panel):
-    chart_type = 'line'
+    # Require that subclasses define chart_type property
+    @abc.abstractproperty
+    def chart_type(self):
+        print("Chart class should never be instantiated, only extended")
+        raise NotImplementedError
 
     def __init__(self, title=None, labels=None, max_points=0):
-        super(LineChart, self).__init__()
+        super(Chart, self).__init__()
         self.title = title
         self.labels = labels
         self.max_points = max_points
@@ -35,5 +25,16 @@ class LineChart(Panel):
         return render_template('chart.js', id=self.id, chart_type=self.chart_type, labels=self.labels,
                                max_points=self.max_points)
 
-    def add_data(self, x, y):
-        self.emit(['add', [x, y]])
+
+class BarChart(Chart):
+    chart_type = 'bar'
+
+    def add_data(self, label, value):
+        self.emit(['add', [label, value]])
+
+
+class LineChart(Chart):
+    chart_type = 'line'
+
+    def add_data(self, label, value):
+        self.emit(['add', [label, value]])
