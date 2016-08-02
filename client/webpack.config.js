8535171909b5a09d7da787c82e4a553d5cd21723
Mark Brown <mark@mtbrown.me>
2016-08-01 22:21:01 -0700

    Added basic front-end client structure and relevant .gitignore entries

---
+++ client/webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    path: 'public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devServer: {
+      contentBase: "./public",
+      hot: true
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
