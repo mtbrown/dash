3620f752d939bb56977f1ec972936b20609c7f3d
Mark Brown <mark@mtbrown.me>
2016-08-02 17:40:26 -0700

    Modified Navbar theme and added relevant props

--- client/app/components/Navbar.jsx
+++ client/app/components/Navbar.jsx
@@ -2,10 +2,34 @@ import React from 'react';
 
 export class Navbar extends React.Component {
    render() {
+      var navbarItemNodes = this.props.items.map(function(item, i) {
+         return <NavbarItem text={item.text} key={i} />;
+      });
+
       return (
-         <div className="ui pointing menu">
-            <NavbarItem text="Home" />
-            <NavbarItem text="Settings" />
+         <div className="ui top fixed inverted menu">
+            <div className="header large item">{ this.props.title }</div>
+            { navbarItemNodes }
+            <div className="right menu">
+               <DropdownMenu items={this.props.dropdownItems} />
+            </div>
+         </div>
+      );
+   }
+}
+
+class DropdownMenu extends React.Component {
+   render () {
+      var dropdownItemNodes = this.props.items.map(function(item, i) {
+         return <NavbarItem text={item.text} key={i} />;
+      });
+
+      return (
+         <div className="ui simple dropdown icon item">
+            <i className="wrench icon"></i>
+            <div className="menu">
+               { dropdownItemNodes }
+            </div>
          </div>
       );
    }
