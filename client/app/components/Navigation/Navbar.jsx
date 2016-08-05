3867336955b65485fd55905867f961ec434f9c36
Mark Brown <mark@mtbrown.me>
2016-08-05 16:26:33 -0700

    Refactored Navbar into smaller components

--- client/app/components/Navigation/Navbar.jsx
+++ client/app/components/Navigation/Navbar.jsx
@@ -1,149 +1,153 @@
 import React from 'react';
-
+import classNames from 'classnames';
 
 export class Navbar extends React.Component {
   render() {
     return (
       <div>
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <a className="navbar-brand" href="index.html">Dash</a>
-        </div>
+        <NavbarHeader />
 
         <ul className="nav navbar-top-links navbar-right">
-          <li className="dropdown">
-            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
-              <i className="fa fa-envelope fa-fw"></i>  <i className="fa fa-caret-down"></i>
-            </a>
-            <ul className="dropdown-menu dropdown-messages">
-              <li>
-                <a href="#">
-                  <div>
-                    <strong>John Smith</strong>
-                    <span className="pull-right text-muted">
-                                        <em>Yesterday</em>
-                                    </span>
-                  </div>
-                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <strong>John Smith</strong>
-                    <span className="pull-right text-muted">
-                                        <em>Yesterday</em>
-                                    </span>
-                  </div>
-                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <strong>John Smith</strong>
-                    <span className="pull-right text-muted">
-                                        <em>Yesterday</em>
-                                    </span>
-                  </div>
-                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a className="text-center" href="#">
-                  <strong>Read All Messages</strong>
-                  <i className="fa fa-angle-right"></i>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li className="dropdown">
-            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
-              <i className="fa fa-tasks fa-fw"></i>  <i className="fa fa-caret-down"></i>
-            </a>
-          </li>
-          <li className="dropdown">
-            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
-              <i className="fa fa-bell fa-fw"></i>  <i className="fa fa-caret-down"></i>
-            </a>
-            <ul className="dropdown-menu dropdown-alerts">
-              <li>
-                <a href="#">
-                  <div>
-                    <i className="fa fa-comment fa-fw"></i> New Comment
-                    <span className="pull-right text-muted small">4 minutes ago</span>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <i className="fa fa-twitter fa-fw"></i> 3 New Followers
-                    <span className="pull-right text-muted small">12 minutes ago</span>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <i className="fa fa-envelope fa-fw"></i> Message Sent
-                    <span className="pull-right text-muted small">4 minutes ago</span>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <i className="fa fa-tasks fa-fw"></i> New Task
-                    <span className="pull-right text-muted small">4 minutes ago</span>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a href="#">
-                  <div>
-                    <i className="fa fa-upload fa-fw"></i> Server Rebooted
-                    <span className="pull-right text-muted small">4 minutes ago</span>
-                  </div>
-                </a>
-              </li>
-              <li className="divider"></li>
-              <li>
-                <a className="text-center" href="#">
-                  <strong>See All Alerts</strong>
-                  <i className="fa fa-angle-right"></i>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li className="dropdown">
-            <a className="dropdown-toggle" data-toggle="dropdown" href="#">
-              <i className="fa fa-user fa-fw"></i>  <i className="fa fa-caret-down"></i>
-            </a>
-            <ul className="dropdown-menu dropdown-user">
-              <li><a href="#"><i className="fa fa-user fa-fw"></i> User Profile</a>
-              </li>
-              <li><a href="#"><i className="fa fa-gear fa-fw"></i> Settings</a>
-              </li>
-              <li className="divider"></li>
-              <li><a href="login.html"><i className="fa fa-sign-out fa-fw"></i> Logout</a>
-              </li>
-            </ul>
-          </li>
+          <NavbarDropdown icon="fa-envelope" className="dropdown-messages">
+            <DropdownMessage
+              author="John Smith"
+              time="Yesterday"
+              message="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+            <DropdownDivider />
+            <DropdownMessage
+              author="John Smith"
+              time="Yesterday"
+              message="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+            <DropdownDivider />
+            <DropdownAction text="Read All Messages" />
+          </NavbarDropdown>
+
+          <NavbarDropdown icon="fa-tasks" className="dropdown-tasks" />
+
+          <NavbarDropdown icon="fa-bell" className="dropdown-alerts">
+            <DropdownAlert icon="fa-comment" text="New Comment" time="4 minutes ago" />
+            <DropdownDivider />
+            <DropdownAlert icon="fa-twitter" text="3 New Followers" time="12 minutes ago" />
+            <DropdownDivider />
+            <DropdownAlert icon="fa-envelope" text="Message Sent" time="18 minutes ago" />
+            <DropdownDivider />
+            <DropdownAction text="See All Alerts" />
+          </NavbarDropdown>
+
+          <NavbarUserMenu />
         </ul>
       </div>
+    );
+  }
+}
+
+
+class NavbarHeader extends React.Component {
+  render() {
+    return (
+      <div className="navbar-header">
+        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+        <a className="navbar-brand" href="index.html">Dash</a>
+      </div>
+    );
+  }
+}
+
+
+class NavbarUserMenu extends React.Component {
+  render() {
+    return (
+      <NavbarDropdown icon="fa-user" className="dropdown-user">
+        <li><a href="#"><i className="fa fa-user fa-fw"></i> User Profile</a>
+        </li>
+        <li><a href="#"><i className="fa fa-gear fa-fw"></i> Settings</a>
+        </li>
+        <li className="divider"></li>
+        <li><a href="login.html"><i className="fa fa-sign-out fa-fw"></i> Logout</a>
+        </li>
+      </NavbarDropdown>
+    );
+  }
+}
+
+
+class NavbarDropdown extends React.Component {
+  render() {
+    let iconClass = classNames('fa', this.props.icon, 'fa-fw');
+    let dropdownClass = classNames('dropdown-menu', this.props.className);
+    return (
+      <li className="dropdown">
+        <a className="dropdown-toggle" data-toggle="dropdown" href="#">
+          <i className={iconClass}></i>  <i className="fa fa-caret-down"></i>
+        </a>
+        <ul className={dropdownClass}>
+          {this.props.children}
+        </ul>
+      </li>
+    );
+  }
+}
+
+
+class DropdownDivider extends React.Component {
+  render() {
+    return <li className="divider"></li>;
+  }
+}
+
+
+class DropdownAction extends React.Component {
+  render() {
+    return (
+      <li>
+        <a className="text-center" href="#">
+          <strong>{this.props.text}</strong>
+          <i className="fa fa-angle-right"></i>
+        </a>
+      </li>
+    );
+  }
+}
+
+
+class DropdownMessage extends React.Component {
+  render() {
+    return (
+      <li>
+        <a href="#">
+          <div>
+            <strong>{this.props.author}</strong>
+            <span className="pull-right text-muted">
+              <em>{this.props.time}</em>
+            </span>
+          </div>
+          <div>{this.props.message}</div>
+        </a>
+      </li>
+    );
+  }
+}
+
+
+class DropdownAlert extends React.Component {
+  render() {
+    let iconClass = classNames('fa', this.props.icon, 'fa-fw');
+    return (
+      <li>
+        <a href="#">
+          <div>
+            <i className={iconClass}></i> {this.props.text}
+            <span className="pull-right text-muted small">{this.props.time}</span>
+          </div>
+        </a>
+      </li>
     );
   }
 }
