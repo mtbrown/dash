dddfdf107f9b5b07235da012e2f978e5df23e73d
Mark Brown <mark@mtbrown.me>
2016-08-04 17:03:37 -0700

    Refactored Sidebar to support dynamic content, added classnames dependency

--- client/app/components/Sidebar.jsx
+++ client/app/components/Sidebar.jsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import classNames from 'classnames';
+
+var scriptList = [
+   {id: "thermometer", title: "Thermometer", status: "ok", notificationCount: 8},
+   {id: "podcasts", title: "Podcasts", status: "ok", notificationCount: 0},
+   {id: "yt_archive", title: "YouTube Archive", status: "ok", notificationCount: 3},
+   {id: "test", title: "Test", status: "error", notificationCount: 7},
+   {id: "wow", title: "Wow", status: "ok", notificationCount: 12}
+];
 
 
 export class Sidebar extends React.Component {
@@ -6,7 +15,7 @@ export class Sidebar extends React.Component {
       return (
          <div className="ui large left vertical visible menu inverted sidebar">
             <SidebarTitle />
-            <ScriptListMenu />
+            <ScriptListMenu scriptList={scriptList} />
          </div>
       );
    }
@@ -27,20 +36,43 @@ class SidebarTitle extends React.Component {
 
 
 class ScriptListMenu extends React.Component {
-   render () {
+   render() {
+      var scriptItemNodes = this.props.scriptList.map(function(script) {
+         return (
+            <ScriptListItem
+               active={false}
+               label={script.notificationCount}
+               color={script.status == "ok" ? 'teal' : 'red'}
+               url={'/scripts/' + script.id + '/'}
+               text={script.title}
+               key={script.id}
+            />
+         );
+      });
+
       return (
          <div className="item">
             <h3>Scripts</h3>
             <div className="menu">
-               <a className="active item" href="/introduction/integrations.html">
-                  Integrations
-                  <div className="ui teal left pointing label">1</div>
-               </a>
-               <a className="item" href="/introduction/integrations.html">
-                  Build Tools
-               </a>
+               { scriptItemNodes }
             </div>
          </div>
+      );
+   }
+}
+
+
+class ScriptListItem extends React.Component {
+   render() {
+      var itemClass = classNames({'active': this.props.active}, 'item');
+      var labelClass = classNames('ui', this.props.color,
+         {'left pointing': this.props.active}, 'label');
+
+      return (
+         <a className={itemClass} href={this.props.url}>
+            {this.props.text}
+            <div className={labelClass}>{this.props.label}</div>
+         </a>
       );
    }
 }
