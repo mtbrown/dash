dca060798194035fc3cd97e99d4eb71767050f1d
Mark Brown <mark@mtbrown.me>
2016-08-11 19:05:29 -0700

    Added basic groundwork for socketio communication between server and client

--- client/app/components/App.jsx
+++ client/app/components/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 import {Navigation} from './Navigation/Navigation.jsx';
-import {Content} from './Views/Content.jsx';
 
+export const socket = io.connect();
 
 export class App extends React.Component {
+  constructor() {
+    super();
+    socket.on('connect', () => {
+      console.log("Connected successfully");
+    })
+  }
+
   render() {
     return (
       <div>
