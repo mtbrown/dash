842099b29af22dbf73b2099d24e774d4d051c135
Mark Brown <mark@mtbrown.me>
2016-08-14 12:43:16 -0700

    Moved sidebar collapse logic to Navigation component

--- client/app/modules/navigation/Navigation.jsx
+++ client/app/modules/navigation/Navigation.jsx
@@ -4,6 +4,37 @@ import { Navbar } from './Navbar.jsx';
 import { Sidebar } from './Sidebar.jsx';
 
 export class Navigation extends React.Component {
+  constructor() {
+    super();
+
+    $(function() {
+      $('#side-menu').metisMenu();
+    });
+
+    //Loads the correct sidebar on window load,
+    //collapses the sidebar on window resize.
+    // Sets the min-height of #page-wrapper to window size
+    $(function() {
+      $(window).bind("load resize", function() {
+        var topOffset = 50;
+        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+          $('div.navbar-collapse').addClass('collapse');
+          topOffset = 100; // 2-row-menu
+        } else {
+          $('div.navbar-collapse').removeClass('collapse');
+        }
+
+        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+          $("#page-wrapper").css("min-height", (height) + "px");
+        }
+      });
+    });
+  }
+
   render() {
     return (
       <nav className="navbar navbar-default navbar-static-top" role="navigation" style={{marginBottom: "0"}}>
